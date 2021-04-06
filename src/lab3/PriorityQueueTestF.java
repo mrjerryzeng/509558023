fdf8d99113997bef8b981451bf27ad136b4c4e76
mrjerryzeng <81568897+mrjerryzeng@users.noreply.github.com>
2021-04-07 00:32:29 +0800

    Update PriorityQueueTestF.java

--- src/lab3/PriorityQueueTestF.java
+++ src/lab3/PriorityQueueTestF.java
@@ -15,9 +15,9 @@ public class PriorityQueueTest {
         return Stream.of(
                 Arguments.arguments(new int[] {2,9,1,3},new int[] {1,4,5,9}),
                 Arguments.arguments(new int[] {9,2,7,5},new int[] {2,3,1,9}),
-                Arguments.arguments(new int[] {6,3,5,1},new int[] {1,3,5,6}),
-                Arguments.arguments(new int[] {9,0,1,4},new int[] {0,1,4,9}),
-                Arguments.arguments(new int[] {1,2,8,5},new int[] {1,2,5,8})
+                Arguments.arguments(new int[] {6,3,5,1},new int[] {2,4,6,8}),
+                Arguments.arguments(new int[] {9,0,1,4},new int[] {1,5,3,2}),
+                Arguments.arguments(new int[] {1,2,8,5},new int[] {1,0,0,0})
         );
     }
     @ParameterizedTest(
