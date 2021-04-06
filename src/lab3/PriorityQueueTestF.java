65a5be998262bbd09d4577420d7fffd12c5c445c
mrjerryzeng <81568897+mrjerryzeng@users.noreply.github.com>
2021-04-07 00:56:12 +0800

    Update PriorityQueueTestF.java

--- src/lab3/PriorityQueueTestF.java
+++ src/lab3/PriorityQueueTestF.java
@@ -1,13 +1,23 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import static org.junit.Assert.assertArrayEquals;
 
 public class PriorityQueueTest {
     static Stream<Arguments> stringIntAndListProvider(){
         return Stream.of(
-                Arguments.arguments(new int[] {2,9,1,3},new int[] {1,4,5,9}),
-                Arguments.arguments(new int[] {9,2,7,5},new int[] {2,3,1,9}),
-                Arguments.arguments(new int[] {6,3,5,1},new int[] {2,4,6,8}),
-                Arguments.arguments(new int[] {9,0,1,4},new int[] {1,5,3,2}),
-                Arguments.arguments(new int[] {1,2,8,5},new int[] {1,0,0,0})
+                Arguments.arguments(new int[] {2,9,1,3},new int[] {1,2,3,9}),
+                Arguments.arguments(new int[] {9,2,7,5},new int[] {2,5,7,9}),
+                Arguments.arguments(new int[] {6,3,5,1},new int[] {1,3,5,6}),
+                Arguments.arguments(new int[] {9,0,1,4},new int[] {0,1,4,9}),
+                Arguments.arguments(new int[] {1,2,8,5},new int[] {1,2,5,8})
         );
     }
     @ParameterizedTest(
